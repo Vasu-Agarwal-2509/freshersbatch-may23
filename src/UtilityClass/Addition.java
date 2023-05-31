0437171d3dd28c5970d93310d6d0fbd78ae1a4bf
Vasu-Agarwal-2509 <vasu.agarwal_cs19@gla.ac.in>
2023-05-31 23:31:34 +0530

    Added new class Addition.java

---
+++ src/UtilityClass/Addition.java
@@ -0,0 +1,12 @@
+package UtilityClass;
+
+public class Addition {
+	public static int additionOfTwoNumbers(int a, int b) {
+		return a + b;
+	}
+	public static void main(String args[]) {
+		System.out.println(additionOfTwoNumbers(2, 3));
+		
+	}
+
+}
