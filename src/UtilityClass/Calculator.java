b0f9b9b8ba48357ee932c7c1d262c10b41b4799c
vasujindal0109 <vasujindal0901@gmail.com>
2023-06-01 00:08:23 +0530

    Update calculator.java

--- src/UtilityClass/Calculator.java
+++ src/UtilityClass/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
 	public static void main(String args[]) {
 		System.out.println("Practice GIT");
 		System.out.println("Second Commit");
+		System.out.println("Cloned Commit");
 	}
 
 }
