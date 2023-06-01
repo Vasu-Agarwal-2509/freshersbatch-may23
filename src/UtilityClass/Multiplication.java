638e0e5075f873eb4209ad3b33be7bf279dddca8
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2023-06-01 10:50:39 +0530

    calculation multiplication of two numbers

---
+++ src/UtilityClass/Multiplication.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+import java.util.*;
+public class Multiplication
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int multi = n1*n2;
+        System.out.println(multi);
+    }
+}
